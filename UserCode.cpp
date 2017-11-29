a3860eb870e79ca03b147130260df9ecfd99f441
jrkroeger <jrkroeger@berkeley.edu>
2017-11-28 18:04:43 -0800

    horizontal gain scheduling less aggressive. 3/4 to 1

--- UserCode.cpp
+++ UserCode.cpp
@@ -124,7 +124,7 @@ MainLoopOutput MainLoop(MainLoopInput const &in) {
   rateGyro_corr = in.imuMeasurement.rateGyro - estGyroBias;
   
   //gain scheduling
-  if (loop_count < 5){
+  if (loop_count < 3){
 //    // D gains on each axis
 //    timeConstant_rollRate = d_roll * 1 / (2 - loop_count/5); // [s]
 //    timeConstant_pitchRate = d_pitch * 1 / (2 - loop_count/5);
@@ -136,9 +136,9 @@ MainLoopOutput MainLoop(MainLoopInput const &in) {
 //    timeConstant_yawAngle = p_yaw*1/(2 - loop_count/5); // [s] (CHANGED! 5.1.2, 1.0f->0.2f)
 
     // time constant for horizontal controller:
-    timeConst_horizVel = h_vel*1/(2 - loop_count/5); //2.0
-    timeConst_horizPos_1 = h_pos1*1/(2 - loop_count/5);
-    timeConst_horizPos_2 = h_pos2*1/(2 - loop_count/5);
+    timeConst_horizVel = h_vel*1/(1.5 - loop_count/6); //2.0
+    timeConst_horizPos_1 = h_pos1*1/(1.5 - loop_count/6);
+    timeConst_horizPos_2 = h_pos2*1/(1.5 - loop_count/6);
   }
   else{
 //    // time constant for controllers on each axis
